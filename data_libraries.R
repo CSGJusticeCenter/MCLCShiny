8355505cdeeeb171b595103b6c841ed09edc297e
mrobertsCSG <mroberts@csg.org>
2022-03-28 12:03:19 -0400

    update theme

--- data_libraries.R
+++ data_libraries.R
@@ -113,56 +113,56 @@ customTheme <- shinyDashboardThemeDIY(
   ,warningFontColor = "#0F0F0F"
   ,dangerFontColor = "#0F0F0F"
   ,bodyBackColor = "#FFFFFF"
-  
+
   ### header
   ,logoBackColor = "#3C3C3C"
-  
+
   ,headerButtonBackColor = "#3C3C3C"
   ,headerButtonIconColor = "#FFFFFF"
   ,headerButtonBackColorHover = "#DCDCDC"
   ,headerButtonIconColorHover = "#3C3C3C"
-  
+
   ,headerBackColor = "#3C3C3C"
   ,headerBoxShadowColor = ""
   ,headerBoxShadowSize = "0px 0px 0px"
-  
+
   ### sidebar
   ,sidebarBackColor = "#3C3C3C"
   ,sidebarPadding = "0"
-  
+
   ,sidebarMenuBackColor = "transparent"
   ,sidebarMenuPadding = "0"
   ,sidebarMenuBorderRadius = 0
-  
+
   ,sidebarShadowRadius = ""
   ,sidebarShadowColor = "0px 0px 0px"
-  
+
   ,sidebarUserTextColor = "#737373"
-  
+
   ,sidebarSearchBackColor = "#FFFFFF"
   ,sidebarSearchIconColor = "#646464"
   ,sidebarSearchBorderColor = "#DCDCDC"
-  
+
   ,sidebarTabTextColor = "#FFFFFF"
   ,sidebarTabTextSize = "14"
   ,sidebarTabBorderStyle = "none"
   ,sidebarTabBorderColor = "none"
   ,sidebarTabBorderWidth = "0"
-  
+
   ,sidebarTabBackColorSelected = "#E6E6E6"
   ,sidebarTabTextColorSelected = "#2D2D2D"
   ,sidebarTabRadiusSelected = "0px"
-  
+
   ,sidebarTabBackColorHover = "#F5F5F5"
   ,sidebarTabTextColorHover = "#2D2D2D"
   ,sidebarTabBorderStyleHover = "none solid none none"
   ,sidebarTabBorderColorHover = "#C8C8C8"
   ,sidebarTabBorderWidthHover = "4"
   ,sidebarTabRadiusHover = "0px"
-  
+
   ### boxes
   ,boxBackColor = "#FFFFFF"
-  ,boxBorderRadius = "3"
+  ,boxBorderRadius = "0"
   ,boxShadowSize = "none"
   ,boxShadowColor = ""
   ,boxTitleSize = "14"
@@ -172,34 +172,134 @@ customTheme <- shinyDashboardThemeDIY(
   ,boxSuccessColor = "#70AD47"
   ,boxWarningColor = "#2D2D2D"
   ,boxDangerColor = "#2D2D2D"
-  
+
   ,tabBoxTabColor = "#F8F8F8"
   ,tabBoxTabTextSize = "14"
   ,tabBoxTabTextColor = "#646464"
   ,tabBoxTabTextColorSelected = "#2D2D2D"
   ,tabBoxBackColor = "#FFFFFF"
   ,tabBoxHighlightColor = "#C8C8C8"
-  ,tabBoxBorderRadius = "2"
-  
+  ,tabBoxBorderRadius = "0"
+
   ### inputs
   ,buttonBackColor = "#D7D7D7"
   ,buttonTextColor = "#2D2D2D"
   ,buttonBorderColor = "#969696"
-  ,buttonBorderRadius = "5"
-  
+  ,buttonBorderRadius = "0"
+
   ,buttonBackColorHover = "#BEBEBE"
-  ,buttonTextColorHover = "#000000"
+  ,buttonTextColorHover = "#2D2D2D"
   ,buttonBorderColorHover = "#969696"
-  
+
   ,textboxBackColor = "#FFFFFF"
   ,textboxBorderColor = "#767676"
-  ,textboxBorderRadius = "5"
+  ,textboxBorderRadius = "0"
   ,textboxBackColorSelect = "#F5F5F5"
   ,textboxBorderColorSelect = "#6C6C6C"
-  
+
   ### tables
-  ,tableBackColor = "#F8F8F8"
-  ,tableBorderColor = "#EEEEEE"
+  ,tableBackColor = "#FFFFFF"
+  ,tableBorderColor = "#FFFFFF"
   ,tableBorderTopSize = "1"
   ,tableBorderRowSize = "1"
 )
+# customTheme <- shinyDashboardThemeDIY(
+#   ### general
+#   appFontFamily = "Arial"
+#   ,appFontColor = "#2D2D2D"
+#   ,primaryFontColor = "#0F0F0F"
+#   ,infoFontColor = "#0F0F0F"
+#   ,successFontColor = "#0F0F0F"
+#   ,warningFontColor = "#0F0F0F"
+#   ,dangerFontColor = "#0F0F0F"
+#   ,bodyBackColor = "#FFFFFF"
+#
+#   ### header
+#   ,logoBackColor = "#3C3C3C"
+#
+#   ,headerButtonBackColor = "#3C3C3C"
+#   ,headerButtonIconColor = "#FFFFFF"
+#   ,headerButtonBackColorHover = "#DCDCDC"
+#   ,headerButtonIconColorHover = "#3C3C3C"
+#
+#   ,headerBackColor = "#3C3C3C"
+#   ,headerBoxShadowColor = ""
+#   ,headerBoxShadowSize = "0px 0px 0px"
+#
+#   ### sidebar
+#   ,sidebarBackColor = "#3C3C3C"
+#   ,sidebarPadding = "0"
+#
+#   ,sidebarMenuBackColor = "transparent"
+#   ,sidebarMenuPadding = "0"
+#   ,sidebarMenuBorderRadius = 0
+#
+#   ,sidebarShadowRadius = ""
+#   ,sidebarShadowColor = "0px 0px 0px"
+#
+#   ,sidebarUserTextColor = "#737373"
+#
+#   ,sidebarSearchBackColor = "#FFFFFF"
+#   ,sidebarSearchIconColor = "#646464"
+#   ,sidebarSearchBorderColor = "#DCDCDC"
+#
+#   ,sidebarTabTextColor = "#FFFFFF"
+#   ,sidebarTabTextSize = "14"
+#   ,sidebarTabBorderStyle = "none"
+#   ,sidebarTabBorderColor = "none"
+#   ,sidebarTabBorderWidth = "0"
+#
+#   ,sidebarTabBackColorSelected = "#E6E6E6"
+#   ,sidebarTabTextColorSelected = "#2D2D2D"
+#   ,sidebarTabRadiusSelected = "0px"
+#
+#   ,sidebarTabBackColorHover = "#F5F5F5"
+#   ,sidebarTabTextColorHover = "#2D2D2D"
+#   ,sidebarTabBorderStyleHover = "none solid none none"
+#   ,sidebarTabBorderColorHover = "#C8C8C8"
+#   ,sidebarTabBorderWidthHover = "4"
+#   ,sidebarTabRadiusHover = "0px"
+#
+#   ### boxes
+#   ,boxBackColor = "#FFFFFF"
+#   ,boxBorderRadius = "3"
+#   ,boxShadowSize = "none"
+#   ,boxShadowColor = ""
+#   ,boxTitleSize = "14"
+#   ,boxDefaultColor = "#2D2D2D"
+#   ,boxPrimaryColor = "#5F9BD5"
+#   ,boxInfoColor = "#C8C8C8"
+#   ,boxSuccessColor = "#70AD47"
+#   ,boxWarningColor = "#2D2D2D"
+#   ,boxDangerColor = "#2D2D2D"
+#
+#   ,tabBoxTabColor = "#F8F8F8"
+#   ,tabBoxTabTextSize = "14"
+#   ,tabBoxTabTextColor = "#646464"
+#   ,tabBoxTabTextColorSelected = "#2D2D2D"
+#   ,tabBoxBackColor = "#FFFFFF"
+#   ,tabBoxHighlightColor = "#C8C8C8"
+#   ,tabBoxBorderRadius = "2"
+#
+#   ### inputs
+#   ,buttonBackColor = "#D7D7D7"
+#   ,buttonTextColor = "#2D2D2D"
+#   ,buttonBorderColor = "#969696"
+#   ,buttonBorderRadius = "5"
+#
+#   ,buttonBackColorHover = "#BEBEBE"
+#   ,buttonTextColorHover = "#000000"
+#   ,buttonBorderColorHover = "#969696"
+#
+#   ,textboxBackColor = "#FFFFFF"
+#   ,textboxBorderColor = "#767676"
+#   ,textboxBorderRadius = "5"
+#   ,textboxBackColorSelect = "#F5F5F5"
+#   ,textboxBorderColorSelect = "#6C6C6C"
+#
+#   ### tables
+#   ,tableBackColor = "#F8F8F8"
+#   ,tableBorderColor = "#EEEEEE"
+#   ,tableBorderTopSize = "1"
+#   ,tableBorderRowSize = "1"
+# )
