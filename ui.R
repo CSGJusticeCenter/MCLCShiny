6446ba224cfe3f78cef5cb9850dc236421cb72ba
mrobertsCSG <mroberts@csg.org>
2022-06-06 16:48:41 -0400

    change navbar header width

--- ui.R
+++ ui.R
@@ -12,7 +12,11 @@ source("dataframes.R")
 source("functions.R")
 
 ui <- fluidPage(includeCSS("www/theme.css"),
-                navbarPage(title = "",
+                navbarPage(tags$style(type = "text/css", ".container-fluid {padding-left:0px; padding-right:0px;}"),
+                           tags$style(type = "text/css", ".navbar {margin-bottom: .5px;}"),
+                           tags$style(type = "text/css", ".container-fluid .navbar-header .navbar-brand {margin-left: 0px;}"),
+
+                           title = "",
 
                            tabPanel("National Report"),
 
@@ -26,9 +30,6 @@ ui <- fluidPage(includeCSS("www/theme.css"),
                                         # Dropdown menus
                                         #######
 
-                                        # tags$style(type="text/css", "#data_map {background-color:#DEF0F6}"),
-                                        # tags$style(type="text/css", "#adm_or_pop_map {background-color:#DEF0F6}"),
-                                        # tags$style(type="text/css", "#year_map {background-color:#DEF0F6}"),
                                         labeled_input('data-map-btn', "Select Data",
                                                       selectizeInput('data_map', label = NULL,
                                                                      choices = c("Total", "New Offense", "Supervision Violation", "Probation Violation", "Parole Violation", "Technical Violation"),
@@ -48,9 +49,9 @@ ui <- fluidPage(includeCSS("www/theme.css"),
 
                                         tags$style(type="text/css", "#save_map {background-color:#004270; color: #fff;}"),
                                         tags$style(type="text/css", "#save_map_data {background-color:#004270; color: #fff;}"),
-                                        labeled_input('save-map-btn', "Download Map",
+                                        labeled_input('save-map-btn', "",
                                                       downloadButton(outputId = 'save_map', label = "Download Map", class = "download_this")),
-                                        labeled_input('save-map-data-btn', "Download Data",
+                                        labeled_input('save-map-data-btn', "",
                                                       downloadButton(outputId = 'save_map_data', label = "Download Data", class = "download_this"))),
 
                                     br()
