19f2fdd23d7fe684ad3ae56cbb41913abaf3f94b
mrobertsCSG <mroberts@csg.org>
2022-06-06 16:04:13 -0400

    update header

--- app.R
+++ app.R
@@ -2,9 +2,13 @@
 # Project: MCLCShiny
 # File: app.R
 # Authors: Mari Roberts
-# Date: April 27, 2022
+# Date last updated: June 6, 2022
 # Description:
 #    Run ui and server
 #######################################
 
+source("ui.R")
+source("server.R")
+
 shinyApp(ui = ui, server = server)
+
--- ui.R
+++ ui.R
@@ -11,96 +11,60 @@ source("library.R")
 source("dataframes.R")
 source("functions.R")
 
-# builds theme object to be supplied to ui
-my_theme <- bs_theme(
-  bootswatch = "cosmo",
-  base_font = font_google("Noto Sans")
-) %>%
-  bs_add_rules(sass::sass_file("styles.scss"))
+ui <- fluidPage(includeCSS("www/theme.css"),
+                navbarPage(title = "",
 
-# let thematic know to use the font from bs_lib
-thematic_shiny(font = "auto")
+                           tabPanel("National Report"),
 
-##################
-# User Interface
-##################
+                           tabPanel("Map Explorer",
 
-ui <- fluidPage(
-  theme = my_theme,
+                                    # div(id = "app-title", titlePanel("Map Explorer")),
 
-  #____________________________________________________________________________________________________________
-  # MAP EXPLORER
-  #____________________________________________________________________________________________________________
+                                    div(id = "header",
 
-  div(id = "app-title",
-      titlePanel("Map Explorer")
-  ),
-  br(),
-  div(id = "header",
-      #######
-      # Dropdown menus
-      #######
-      # tags$style(type="text/css", "#data_map {background-color:#DEF0F6}"),
-      # tags$style(type="text/css", "#adm_or_pop_map {background-color:#DEF0F6}"),
-      # tags$style(type="text/css", "#year_map {background-color:#DEF0F6}"),
-      labeled_input('data-map-btn', "Data",
-                    selectizeInput('data_map', label = NULL,
-                                   choices = c("Total", "New Offense", "Supervision Violation", "Probation Violation", "Parole Violation", "Technical Violation"),
-                                   multiple = FALSE)),
-      labeled_input('adm-pop-map-btn', "Admissions or Population",
-                    selectizeInput('adm_or_pop_map', label = NULL,
-                                   choices = c("Admissions", "Population"),
-                                   multiple = FALSE)),
-      labeled_input('year-map-btn', "Year",
-                    selectizeInput('year_map', label = NULL,
-                                   choices = c("2018 - 2019", "2019 - 2020"),
-                                   multiple = FALSE)),
+                                        #######
+                                        # Dropdown menus
+                                        #######
 
-      #######
-      # Download buttons
-      #######
-      tags$style(type="text/css", "#save_map {background-color:#355DA1}"),
-      tags$style(type="text/css", "#save_map_data {background-color:#355DA1}"),
-      labeled_input('save-map-btn', "Download Map",
-                    downloadButton(outputId = 'save_map', label = "Download Map", class = "download_this")),
-      labeled_input('save-map-data-btn', "Download Data",
-                    downloadButton(outputId = 'save_map_data', label = "Download Data", class = "download_this"))
-  ),
-  br(),
+                                        # tags$style(type="text/css", "#data_map {background-color:#DEF0F6}"),
+                                        # tags$style(type="text/css", "#adm_or_pop_map {background-color:#DEF0F6}"),
+                                        # tags$style(type="text/css", "#year_map {background-color:#DEF0F6}"),
+                                        labeled_input('data-map-btn', "Select Data",
+                                                      selectizeInput('data_map', label = NULL,
+                                                                     choices = c("Total", "New Offense", "Supervision Violation", "Probation Violation", "Parole Violation", "Technical Violation"),
+                                                                     multiple = FALSE)),
+                                        labeled_input('adm-pop-map-btn', "Select Type",
+                                                      selectizeInput('adm_or_pop_map', label = NULL,
+                                                                     choices = c("Admissions", "Population"),
+                                                                     multiple = FALSE)),
+                                        labeled_input('year-map-btn', "Select Years",
+                                                      selectizeInput('year_map', label = NULL,
+                                                                     choices = c("2018 - 2019", "2019 - 2020"),
+                                                                     multiple = FALSE)),
 
-  #######
-  # Hex map
-  #######
+                                        #######
+                                        # Download buttons
+                                        #######
 
-  # div(id = "selected-map",
-  #     textOutput("selected_map")),
-  highchartOutput("hex_map", height = 600, width = 1091),
-  br(),
+                                        tags$style(type="text/css", "#save_map {background-color:#004270; color: #fff;}"),
+                                        tags$style(type="text/css", "#save_map_data {background-color:#004270; color: #fff;}"),
+                                        labeled_input('save-map-btn', "Download Map",
+                                                      downloadButton(outputId = 'save_map', label = "Download Map", class = "download_this")),
+                                        labeled_input('save-map-data-btn', "Download Data",
+                                                      downloadButton(outputId = 'save_map_data', label = "Download Data", class = "download_this"))),
 
-  #######
-  # Hex map table
-  #######
+                                    br()
+                           ),
 
-  div(id = "selected-map-table",
-      textOutput("selected_map_table")),
-  div(id = "table-map",
-  # reactableOutput("table_map"),
-  dataTableOutput("table_map"),
-  ),
+                           tabPanel("State Reports"),
 
-  br(),
+                           tabPanel("Download"),
 
-  div(class = "small_text",
-      icon("database"), "Source:",
-      a(href = "https://csgjusticecenter.org/publications/more-community-less-confinement/", "More Community, Less Confinement (2021)"))
+                           tabPanel("Methodology"),
 
-  #____________________________________________________________________________________________________________
-  # STATE REPORTS
-  #____________________________________________________________________________________________________________
+                           tabPanel("Videos"),
 
-  #____________________________________________________________________________________________________________
-  # DOWNLOAD DATA
-  #____________________________________________________________________________________________________________
-
-
+                           tabPanel("Project Credits"))
 )
+
+
