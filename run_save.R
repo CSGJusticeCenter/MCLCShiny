3a5ea7a004182778a6648ab0948ca6c02e726359
Martha Eichlersmith <meichlersmith@csg.org>
2022-08-24 12:04:44 -0700

    add functions for Rmd and qmd, save outputs on sharepoint and datestamp

---
+++ run_save.R
@@ -0,0 +1,52 @@
+
+sp_path <- csgjcr::csg_sp_path("50 State Revocations Project/MCLC Shiny App/products")
+
+## save Rmd's to sharepoint path 
+run_Rmd <- function(SP_PATH, IN, OUT){
+  
+  datestamp <- gsub("-", "", Sys.Date())
+  outfile1 <- file.path(SP_PATH, "datestamp", paste0(datestamp, "_", OUT))
+  outfile2 <- file.path(SP_PATH,                        OUT )
+  
+  rmarkdown::render(input = IN, output_file = outfile1)
+  rmarkdown::render(input = IN, output_file = outfile2)
+  file.show(outfile2)
+  
+}
+
+## save qmd's to sharepoint path 
+run_qmd <- function(SP_PATH, IN, OUT){
+  
+  datestamp <- gsub("-", "", Sys.Date())
+  outfile1 <- file.path(SP_PATH, "datestamp", paste0(datestamp, "_", OUT))
+  outfile2 <- file.path(SP_PATH,                        OUT )
+  
+  #https://stackoverflow.com/questions/72346829/two-problems-rendering-a-qmd-file-with-quarto-render-from-r
+  #quarto::quarto_render(input = "prep/NCRP/gen_documentation.qmd", output_file = "prep/NCRP/test/gen_documentation.html")
+  
+  inpath_end <- stringr::str_locate_all(IN, "/")[[1]][,2] |> tail(1)
+  inpath     <- stringr::str_sub(IN, 1, inpath_end)
+  outpath    <- paste0(inpath, OUT)
+  
+  quarto::quarto_render(input = IN, output_file = outpath)
+  file.copy(from = outpath, to = outfile1, overwrite = TRUE)
+  file.copy(from = outpath, to = outfile2, overwrite = TRUE)
+  file.remove(outpath)
+  file.show(outfile2)
+  
+}
+
+
+### RMD's 
+
+run_Rmd(sp_path, "prep/NCRP/NCRP_demo.Rmd", "NCRP_Revocations_Demo.html")
+
+
+### QMD's 
+
+run_qmd(sp_path, "prep/NCRP/gen_documentation.qmd", "General_Documentation.html")
+
+
+
+
+
