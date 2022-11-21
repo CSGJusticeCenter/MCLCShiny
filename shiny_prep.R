dc99755386560cfdaf8fd234088acb507f27e9b1
Martha Eichlersmith <meichlersmith@csg.org>
2022-11-21 16:30:56 -0600

    move shiny prep file to root folder

---
+++ shiny_prep.R
@@ -0,0 +1,36 @@
+
+
+
+Sys.time() #takes ~ 15 min 
+
+my_log <- file("shiny_prep_log.txt") # File name of output log
+
+sink(my_log, append = FALSE, type = "output") # Writing console output to log file
+sink(my_log, append = FALSE, type = "message")
+
+########## takes ~ 1min
+Sys.time()
+source("import.R", echo = TRUE)
+
+
+####### takes ~ 2min 
+Sys.time()
+source("highchart.R", echo = TRUE)
+
+
+
+####### takes ~ 1min 
+Sys.time()
+source("reactable.R", echo = TRUE)
+
+####### takes ~ 10min
+# working directory should be the root of the repository ~MCLCShiny/
+Sys.time()
+box::use(prep/box/rri_infographs_tables)
+rri_infographs_tables$prep_for_shiny("N")
+
+warnings()
+
+#closeAllConnections() # Close connection to log file
+
+Sys.time()
