62b3cdb912b5d405dcc43d96c49a323f94e6a139
Martha Eichlersmith <meichlersmith@csg.org>
2022-11-22 09:38:58 -0600

    add PUMS script from Matt Herman (pulls down PUMS data to match NCRP race categories)

---
+++ prep/get-ncrp-base-rate-acs-pums.R
@@ -0,0 +1,71 @@
+# Code written by Matt Herman 
+# Slight edits by Martha Eichlersmith (add 2015, export to sharepoint using admin$sp_data_raw path)
+# 2022-11-22
+# run this script once to create the PUMS data that will be used 
+# save output to SharePoint 
+
+Sys.time() #takes ~10 minutes 
+
+library(tidycensus)
+library(tidyverse)
+
+box::use(
+  box/admin
+)
+
+# get state code and names from pums to recode after we download
+st_recode <- tidycensus::pums_variables |>
+  filter(survey == "acs1", var_code == "ST", year == "2019") |>
+  select(ST = val_min, val_label) |>
+  separate(val_label, into = "state", sep = "/", extra = "drop")  # this is formatted weirdly, so let's just use the state name
+
+# define function to pull race and ethnicity variables from pums api
+get_pums_race_eth <- function(year, state = "all") {
+  get_pums(
+    variables = c("AGEP", "HISP", "RACBLK", "RAC1P"),
+    state = state,
+    year = year,
+    survey = "acs1"
+    )
+  }
+
+# define function to calculate new combined race/eth var for ncrp base rate
+calc_ncrp_race <- function(df) {
+  df |>
+    filter(AGEP >= 18) |>                                     # adults only
+    mutate(
+      ncrp_race = case_when(
+         HISP %in% c("1", "01") & RAC1P  == "1" ~ "White",    # not hispanic and white alone
+        !HISP %in% c("1", "01") & RACBLK == "0" ~ "Hispanic", # hispanic, not black
+        RACBLK == "1"                           ~ "Black",    # black alone or in combination and any ethnicity
+        TRUE                                    ~ "Other"     # all others
+      )
+    ) |>
+    count(ST, ncrp_race, wt = PWGTP) |>                       # count number of people by new race, using survey person weight
+    group_by(ST) |>
+    mutate(pct = n / sum(n)) |>                               # pct race/eth by state
+    ungroup()
+  }
+
+# for each year, download race and eth vars from pums
+# warning: this takes a long time because it hits the api for each state for each year
+# (could set up an ipums download alternatively, but i don't love the ipums interface)
+pums <- map(2015:2019, get_pums_race_eth)
+
+# calculate new race variable for each year and combine into a single df
+# join with state lookup to get state names from pums state codes
+ncrp_race_by_state <- pums |>
+  set_names(2015:2019) |>
+  map_dfr(calc_ncrp_race, .id = "year") |>
+  mutate(
+    ST = str_pad(ST, 2, pad = "0"),   # 2016 has some wonky codes that need to be padded with 0s to recode
+    year = as.numeric(year)
+    ) |>
+  left_join(st_recode, by = "ST") |>
+  select(-ST) |>
+  arrange(year, state, ncrp_race)
+
+# write to file
+write_rds(ncrp_race_by_state, file.path(admin$sp_data_raw, "PUMS/ncrp_race_by_state.RDS"))
+
+Sys.time()
