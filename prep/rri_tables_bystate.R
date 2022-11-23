e0f43a035c6faa10d5c5b11f27b4c3b54f09a7f9
Martha Eichlersmith <meichlersmith@csg.org>
2022-11-23 10:18:55 -0600

    generalize csv rri table csv export

--- prep/rri_tables_bystate.R
+++ prep/rri_tables_bystate.R
@@ -4,37 +4,41 @@ library(tidyverse)
 
 box::use(
   box/admin
+  , glue[glue]
 )
 
-REV_APS <- readRDS(file.path(admin$sp_data, "NCRP_REV_APS.RDS"))
-REV_SC <- readRDS(file.path(admin$sp_data, "NCRP_REV_SC.RDS"))
+NCRPLET <- "N"
+CENPOP  <- "PUMS"
+
+REV_BJS <- readRDS(file.path(admin$sp_data, glue("NCRP_{NCRPLET}_REV_BJS.RDS")))
+REV_CEN <- readRDS(file.path(admin$sp_data, glue("NCRP_{NCRPLET}_REV_{CENPOP}.RDS")))
 
 
 bind_rows(
-  REV_APS$R %>% 
+  REV_BJS$R %>% 
     filter(RACE %in% admin$lev_RACE[1:3]) %>% #only look at White, Black, Hispanic
     filter(RPTYEAR == RECENT_YR) %>%  #most recent year
-    mutate(OFFGENERAL = "All categories", denom = "BJS - APS") %>% 
-    select(denom, STATE, RPTYEAR, OFFGENERAL, RACE, REVCNT, POPEST, RATE_100K, RRI)
+    mutate(OFFGENERAL = "All categories", data = glue("{NCRPLET} - BJS")) %>% 
+    select(data, STATE, RPTYEAR, OFFGENERAL, RACE, REVCNT, SUPPRESS, POPEST, S_REVCNT, S_RATE, S_RRI)
   , 
-  REV_APS$OR %>% 
+  REV_BJS$OR %>% 
     filter(RACE %in% admin$lev_RACE[1:3], OFFGENERAL %in% admin$lev_OFFGENERAL[1:4]) %>% #only look at White, Black, Hispanic
     filter(RPTYEAR == RECENT_YR) %>%  #most recent year
-    mutate(denom = "BJS - APS") %>% 
-    select(denom, STATE, RPTYEAR, OFFGENERAL, RACE, REVCNT, POPEST, RATE_100K, RRI)
+    mutate(data = glue("{NCRPLET} - BJS")) %>% 
+    select(data, STATE, RPTYEAR, OFFGENERAL, RACE, REVCNT, SUPPRESS, POPEST, S_REVCNT, S_RATE, S_RRI)
 ) %>% write_csv(file.path(admin$sp_data, "RRI_rates_BJSdenom.csv"))
 
 
 bind_rows(
-  REV_SC$R %>% 
+  REV_CEN$R %>% 
     filter(RACE %in% admin$lev_RACE[1:3]) %>% #only look at White, Black, Hispanic
     filter(RPTYEAR == RECENT_YR) %>%  #most recent year
-    mutate(OFFGENERAL = "All categories", denom = "Census - SC") %>% 
-    select(denom, STATE, RPTYEAR, OFFGENERAL, RACE, REVCNT, POPEST, RATE_100K, RRI)
+    mutate(OFFGENERAL = "All categories", data = glue("{NCRPLET} - {CENPOP}")) %>% 
+    select(data, STATE, RPTYEAR, OFFGENERAL, RACE, REVCNT, SUPPRESS, POPEST, S_REVCNT, S_RATE, S_RRI)
   , 
-  REV_SC$OR %>% 
+  REV_CEN$OR %>% 
     filter(RACE %in% admin$lev_RACE[1:3], OFFGENERAL %in% admin$lev_OFFGENERAL[1:4]) %>% #only look at White, Black, Hispanic
     filter(RPTYEAR == RECENT_YR) %>%  #most recent year
-    mutate(denom = "Census - SC") %>% 
-    select(denom, STATE, RPTYEAR, OFFGENERAL, RACE, REVCNT, POPEST, RATE_100K, RRI)
+    mutate(data = glue("{NCRPLET} - {CENPOP}")) %>% 
+    select(data, STATE, RPTYEAR, OFFGENERAL, RACE, REVCNT, SUPPRESS, POPEST, S_REVCNT, S_RATE, S_RRI)
 ) %>% write_csv(file.path(admin$sp_data, "RRI_rates_Censusdenom.csv"))
