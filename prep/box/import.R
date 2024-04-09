96a90c42b446be9e4021a412f1529ca2f17aaddc
Martha Eichlersmith <meichlersmith@csg.org>
2024-04-09 15:43:07 -0500

    update data pathways to JR_DATA_LIBRARY

--- prep/box/import.R
+++ prep/box/import.R
@@ -9,11 +9,8 @@ box::use(
   , ./admin
 )
 
-# MR
-# CC_path <- csg_sp_path("JC Research - CC/data/raw")
-
 # ME
-CC_path <- csg_sp_path("CC/data/raw")
+datalib <- csg_sp_path("JR_DATA_LIBRARY/data/raw")
 
 
 
@@ -26,15 +23,15 @@ NCRP_ARN <- function(){
   admin$mylog("Start NCRP import, 3 data sets")
 
   #da38048.0002 -- prison admissions
-  load(file.path(CC_path,"NCRP/ICPSR_38492-V1/ICPSR_38492", "DS0002/38492-0002-Data.rda"))
+  load(file.path(datalib,"bjs/ncrp/ICPSR_38492-V1/ICPSR_38492", "DS0002/38492-0002-Data.rda"))
   admin$mylog("Complete NCRP import: admissions")
 
   #da38048.0003 -- prison releases
-  load(file.path(CC_path,"NCRP/ICPSR_38492-V1/ICPSR_38492", "DS0003/38492-0003-Data.rda"))
+  load(file.path(datalib,"bjs/ncrp/ICPSR_38492-V1/ICPSR_38492", "DS0003/38492-0003-Data.rda"))
   admin$mylog("Complete NCRP import: releases")
 
   #da38048.0004 -- yearend population
-  load(file.path(CC_path,"NCRP/ICPSR_38492-V1/ICPSR_38492", "DS0004/38492-0004-Data.rda"))
+  load(file.path(datalib,"bjs/ncrp/ICPSR_38492-V1/ICPSR_38492", "DS0004/38492-0004-Data.rda"))
   admin$mylog("Complete NCRP import: population")
 
   raw <- list(
@@ -61,7 +58,7 @@ NCRP_A <- function(){
   admin$mylog("Start - NCRP import for admissions")
 
   #da38492.0002 -- prison admissions
-  load(file.path(CC_path,"NCRP/ICPSR_38492-V1/ICPSR_38492", "DS0002/38492-0002-Data.rda"))
+  load(file.path(datalib,"bjs/ncrp/ICPSR_38492-V1/ICPSR_38492", "DS0002/38492-0002-Data.rda"))
 
   raw <- da38492.0002
 
@@ -80,7 +77,7 @@ NCRP_N <- function(){
   admin$mylog("Start - NCRP import for year-end population ")
 
   #da38492.0004 -- year end population
-  load(file.path(CC_path,"NCRP/ICPSR_38492-V1/ICPSR_38492", "DS0004/38492-0004-Data.rda"))
+  load(file.path(datalib,"bjs/ncrp/ICPSR_38492-V1/ICPSR_38492", "DS0004/38492-0004-Data.rda"))
 
   raw <- da38492.0004
 
@@ -98,7 +95,7 @@ NCRP_N <- function(){
 SC <- function(){
 
   admin$mylog("Start - SC import")
-  raw <- read_csv(file.path(CC_path, "SC-EST/SC-EST2020-ALLDATA6.csv"), show_col_types = FALSE) %>%
+  raw <- read_csv(file.path(datalib, "census/SC-EST/SC-EST2020-ALLDATA6.csv"), show_col_types = FALSE) %>%
     mutate(POPTYPE = "SC")
   admin$mylog("End   - SC import")
 
@@ -121,23 +118,32 @@ SC <- function(){
 APS <- function(startyr = NA, endyr = NA){
 
 
-  folder <- "AParS"
-  metadata <- read_csv(file.path(CC_path, glue("BJS/{folder}/{tolower(folder)}_idyear.csv")), show_col_types = FALSE)
-  minyr <- ifelse(is.na(startyr), min(metadata$RPTYEAR), startyr)
-  maxyr <- ifelse(is.na(endyr)  , max(metadata$RPTYEAR), endyr)
-  thisdata <- metadata[metadata$RPTYEAR >= minyr & metadata$RPTYEAR <= maxyr, ]
+  folder <- "apars"
+  metadata <- read_csv(file.path(datalib, glue("bjs/{folder}/{tolower(folder)}_idyear.csv")), show_col_types = FALSE)
+  minyr <- ifelse(is.na(startyr), min(metadata$year), startyr)
+  maxyr <- ifelse(is.na(endyr)  , max(metadata$year), endyr)
+  thisdata <- metadata[metadata$year >= minyr & metadata$year <= maxyr, ]
+  
+  
+  rda_pathway <- function(icpsr){
+    paste0("ICPSR_", icpsr, "-V1/ICPSR_", icpsr,"/DS0001/", icpsr, "-0001-Data.rda")
+  }
+  
+  rda_name <- function(icpsr){
+    paste0("da", icpsr, ".0001")
+  }
 
 
   admin$mylog(glue("Start - BJS APS (Parole) import for {minyr}-{maxyr}"))
 
   ## load files into R
-  purrr::walk( thisdata$rda_pathway, ~load(file.path(CC_path, "BJS", folder, .x), .GlobalEnv) )
+  purrr::walk( thisdata$icpsr_id, ~load(file.path(datalib, "bjs", folder, rda_pathway(.x)), .GlobalEnv) )
 
   ## add rptyear and folder (type) to indv year data sets, bind year data sets together
   # need to switch from numeric -> factor, probation (2013, 2015-2018)
   rawcombined <- purrr::map2(
-      thisdata$rda_name[c(1:5)]
-    , thisdata$RPTYEAR[c(1:5)]
+      rda_name(thisdata$icpsr_id[c(1:5)])
+    , thisdata$year[c(1:5)]
     , ~mget(.x, envir = .GlobalEnv)[[1]] %>%
           mutate(
               POPTYPE = folder
@@ -155,7 +161,7 @@ APS <- function(startyr = NA, endyr = NA){
     ) %>% bind_rows() %>% as_tibble()
 
   ## remove originally loaded data frames (indv data frames by year)
-  rm(list = thisdata$rda_name, envir = .GlobalEnv)
+  rm(list = rda_name(thisdata$icpsr_id), envir = .GlobalEnv)
 
   admin$mylog(glue("End   - BJS APS (Parole) import for {minyr}-{maxyr}"))
 
