6e6b869d2124f4a6f1f37dfc98b50ee14ba4b94d
Martha Eichlersmith <meichlersmith@csg.org>
2025-02-26 17:53:30 -0600

    WIP highcharts - hex maps

--- prep/3b_highchart_create.R
+++ prep/3b_highchart_create.R
@@ -38,23 +38,6 @@ metrics <- c("New Offense Violation",
 
 # NATL - HEX MAPS ##############################################################
 
-adm_pop_mapstst <- 
-  map(#lev1 type 
-    c("Admissions", "Population") |> set_names(), 
-    ~{type <- .x
-    map(#lev2 year_chg
-      svii_yr$change_name |> set_names(), 
-      ~{year_chg <- .x
-      map(#lev3 metric 
-        metrics |> set_names(), 
-        ~{metric <- .x
-        #FUNCTION TO CREATE HIGHCHART 
-        paste(type, year_chg, metric)
-        }) #end of lev3
-      }) #end of lev2
-    }) #end of lev1
-
-
 adm_pop_maps <- 
   map(#lev1 type 
     c("Admissions", "Population") |> set_names(), 
@@ -67,6 +50,7 @@ adm_pop_maps <-
         ~{metric <- .x
         #FUNCTION TO CREATE HIGHCHART 
         fnc_hc_hex_map(type, year_chg, metric)
+        # paste(type, year_chg, metric) # <-- if want to test nested list structure 
       }) #end of lev3
     }) #end of lev2
   }) #end of lev1
