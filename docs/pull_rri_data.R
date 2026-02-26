13c3b85eef6aea9bbf9b1d2081940e5123dc7916
Martha Eichlersmith <meichlersmith@csg.org>
2026-02-26 14:40:07 -0600

    create specific export file

---
+++ docs/pull_rri_data.R
@@ -0,0 +1,19 @@
+
+# load box modules 
+box::use(../prep/box/admin)
+
+library(tidyverse)
+library(csgjcr) 
+
+svii_demo <- readRDS("prep/svii_demo.rds")
+
+
+# want sex/gender data 
+
+svii_demo |> 
+  filter(group_cat == "sex_gender") |> 
+  select(state_name, type, data, group, n_avg = n/2, prop, comparison_source, 
+         ncomp, rate, rri) |> 
+  readr::write_csv(file = file.path(admin$sp_data, "svii_rri_sex_gender.csv"))
+
+
